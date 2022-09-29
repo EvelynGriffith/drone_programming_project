e0b6f2a4048182dbf2b8c800373765ff3ddf127a
PCain02 <Cain01@allegheny.edu>
2022-09-29 10:25:50 -0400

    demo edits

--- src/demo.py
+++ src/demo.py
@@ -5,15 +5,16 @@ Land
 from pyparrot.Bebop import Bebop
 
 bebop = Bebop(drone_type="Bebop")
+# bebop.set_hull_protection(1)
 
-print("connecting")
+# print("connecting")
 success = bebop.connect(10)
 print(success)
 
-if (success):
-    bebop.safe_takeoff(5)
-    bebop.smart_sleep(5)
-    bebop.safe_land(5)
+if success:
+    # bebop.safe_takeoff(5)
+    # bebop.smart_sleep(5)
+    # bebop.safe_land(5)
 
     print("DONE - disconnecting")
     bebop.disconnect()
